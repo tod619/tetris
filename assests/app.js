175cda51403d9767c4dbba35b2f5597dae204d37
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-05-18 16:07:24 +0100

    Create variables in app.js file

---
+++ assests/app.js
@@ -0,0 +1,5 @@
+const grid = document.querySelector('.grid');
+let squares = Array.from(document.querySelectorAll('.grid div'));
+const scoreDisplay = document.querySelector('#score');
+const startBtn = document.querySelector('#start-button');
+const width = 10;
