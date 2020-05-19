bfc6e86ead47cc8b8d5aca3cdaee51404787b3eb
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-05-19 09:50:50 +0100

    Create a movedDown function in app.js

--- assests/app.js
+++ assests/app.js
@@ -7,41 +7,41 @@ const width = 10;
 
 // The tetrominoes
 const lTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2 + 1, width * 2 + 2]
+	[ 1, width + 1, width * 2 + 1, 2 ],
+	[ width, width + 1, width + 2, width * 2 + 2 ],
+	[ 1, width + 1, width * 2 + 1, width * 2 ],
+	[ width, width * 2, width * 2 + 1, width * 2 + 2 ]
 ];
 
 const zTetromino = [
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1]
+	[ 0, width, width + 1, width * 2 + 1 ],
+	[ width + 1, width + 2, width * 2, width * 2 + 1 ],
+	[ 0, width, width + 1, width * 2 + 1 ],
+	[ width + 1, width + 2, width * 2, width * 2 + 1 ]
 ];
 
 const tTetromino = [
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1]
+	[ 1, width, width + 1, width + 2 ],
+	[ 1, width + 1, width + 2, width * 2 + 1 ],
+	[ width, width + 1, width + 2, width * 2 + 1 ],
+	[ 1, width, width + 1, width * 2 + 1 ]
 ];
 
 const oTetromino = [
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1]
+	[ 0, 1, width, width + 1 ],
+	[ 0, 1, width, width + 1 ],
+	[ 0, 1, width, width + 1 ],
+	[ 0, 1, width, width + 1 ]
 ];
 
 const iTetromino = [
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3],
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3]
+	[ 1, width + 1, width * 2 + 1, width * 3 + 1 ],
+	[ width, width + 1, width + 2, width + 3 ],
+	[ 1, width + 1, width * 2 + 1, width * 3 + 1 ],
+	[ width, width + 1, width + 2, width + 3 ]
 ];
 
-const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+const theTetrominoes = [ lTetromino, zTetromino, tTetromino, oTetromino, iTetromino ];
 
 let currentPosition = 4;
 let currentRotation = 0;
@@ -52,14 +52,24 @@ let current = theTetrominoes[random][currentRotation];
 
 // Draw the tetrominoes
 function draw() {
-    current.forEach(index => {
-        squares[currentPosition + index].classList.add('tetromino');
-    })
+	current.forEach((index) => {
+		squares[currentPosition + index].classList.add('tetromino');
+	});
 }
 
 // Undraw the tetrominoes
 function undraw() {
-    current.forEach(index => {
-        squares[currentPosition + index].classList.remove('tetromino');
-    })
+	current.forEach((index) => {
+		squares[currentPosition + index].classList.remove('tetromino');
+	});
+}
+
+// Make the tetrominoes move down the grid
+timerId = setInterval(moveDown, 1000);
+
+// moveDown function
+function moveDown() {
+	undraw();
+	currentPosition += width;
+	draw();
 }
